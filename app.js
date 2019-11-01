96753d9656253f37c2517165d578a981cdbc0b20
Mitch Alforque <malforque717@gmail.com>
2019-11-01 18:53:16 +0800

    test js

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 $(function() {
-    var imgFolder = "/includes/img";
-
-    $.get(imgFolder).then(function(result, status) {
-    $(result).find("a").attr('href', function(index, value) {
-        if( value.match(/\.(jpe?g|png|gif)$/) ) { 
-            $('.carousel').append(`<a class="carousel-item" href="#"><img src="`+ value + `"></a>`);
-
-        }
+    $.get("https://photos.google.com/u/2/album/AF1QipMxNBjEVSqT1LzvS4KOsUEDDqPhy7ElTpy22GhX").then(function(result) {
+        console.log(result);
     });
-    }).done(function(){
-        $('.carousel').carousel({
-            onCycleTo: function(data) {
-                var img = $(data).find('img').attr('src');
-                $('.carousel').css('background-image', 'url('+img+')');
-            }
-        });
-    });
+    // var imgFolder = "/includes/img";
+
+    // $.get(imgFolder).then(function(result, status) {
+    // $(result).find("a").attr('href', function(index, value) {
+    //     if( value.match(/\.(jpe?g|png|gif)$/) ) { 
+    //         $('.carousel').append(`<a class="carousel-item" href="#"><img src="`+ value + `"></a>`);
+
+    //     }
+    // });
+    // }).done(function(){
+    //     $('.carousel').carousel({
+    //         onCycleTo: function(data) {
+    //             var img = $(data).find('img').attr('src');
+    //             $('.carousel').css('background-image', 'url('+img+')');
+    //         }
+    //     });
+    // });
 });
